7c9ef1f8f6ac4cd0f89a45b59c4f90a254cdc115
Felipe Pardo <felipepardo90@hotmail.com>
2022-08-21 14:11:53 -0300

    desafío 2

--- desafio2/index.js
+++ desafio2/index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const { json } = require("stream/consumers");
 const file = "./products.json";
 
 class Container {
@@ -10,23 +9,46 @@ class Container {
   async save(object) {
     const dataToParse = await fs.readFileSync(this.file, "utf-8");
     const dataParsed = JSON.parse(dataToParse);
-    // ? console.log(dataToParse)
-    // ? console.log(dataParsed)
+    // ? ¿El producto ya existe en el archivo?
+    const productFound = dataParsed.find(({ title }) => title == object.title);
 
     try {
-      object.id = dataParsed.length + 1;
-      dataParsed.push(object);
-      const updatedFile = JSON.stringify(dataParsed, null, " ");
-      fs.writeFileSync(this.file, updatedFile);
-      // ? console.log(updatedFile, "Aquí 23");
-      console.log(object)
-      return object.id;
+      if (productFound) {
+        // * Si el producto ya existe, avisa por consola y no lo agrega
+        console.log("El producto ya existe");
+      } else {
+        // * Si no existe, lo agrega y retorna el id asignado
+        object.id = dataParsed.length + 1;
+        dataParsed.push(object);
+        const updatedFile = JSON.stringify(dataParsed, null, " ");
+        fs.writeFileSync(this.file, updatedFile);
+        // ? console.log(updatedFile, "Aquí 23");
+        console.log(
+          `Se ha agregado el siguiente producto: ${object.title} con el id ${object.id}`
+        );
+        return object.id;
+      }
     } catch (error) {
-      console.log(`Se produjo un erro ${error}`);
+      console.log(`Se produjo un error ${error}`);
     }
   }
 
-  getById(id) {}
+  async getById(id) {
+    const dataToParse = await fs.readFileSync(this.file, "utf-8");
+    const dataParsed = JSON.parse(dataToParse);
+    // ? ¿El producto ya existe en el archivo?
+    const idFound = dataParsed.find(({ id }) => id == id);
+
+    try {
+      if (idFound) {
+        return idFound
+      } else {
+        null
+      }
+    } catch (error) {
+      console.error(`Se produjo un error en getByID: ${error}`)
+    }
+  }
 
   getAll() {}
 
@@ -44,4 +66,5 @@ let newObject = {
     "https://th.bing.com/th/id/R.d17cfb95d1f5b067f573e89e4ab70e98?rik=okWe3Nn%2b3DZhpg&pid=ImgRaw&r=0",
 };
 
-console.log(contenedor.save(newObject));
+contenedor.save(newObject);
+contenedor.getById(1)
