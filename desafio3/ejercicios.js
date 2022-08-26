4537c6bb67e474a1748ec3e643ae5a2115ad7960
Felipe Pardo <felipepardo90@hotmail.com>
2022-08-25 21:12:49 -0300

    ejercicios semana 3

--- desafio3/ejercicios.js
+++ desafio3/ejercicios.js
@@ -1,9 +1,60 @@
-const numbers = {}
+// const numbers = {}
+
+// const aleatoryNum = ()=>{
+
+//     return parseInt((Math.random()*20)+1)
+// }
+
+// console.log(aleatoryNum())
+
+////////////////////////////////////////// !
+
+///* HTTP
+
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   const date = new Date();
+//   const hora = date.getHours();
+ 
+// });
+
+// const serverConnect = server.listen(8080, () => {
+//   console.log("Servidor Activo");
+// });
+
+////////////////////////////////////////// !
+
+///* EXPRESS
+
+const express = require("express")
+const app = express()
 
 
-const aleatoryNum = ()=>{
+app.get("/", (req, res)=>{
+    res.send("<h1 style='color:blue'> Bienvenidos al servidor express </h1>")
+})
 
-    return parseInt((Math.random()*20)+1)
-}
+let contador = 0
 
-console.log(aleatoryNum())
+app.get("/visitas", (req, res)=>{
+    contador++
+    res.send(`Eres el visitante nro ${contador}`)
+})
+
+app.get("/fyh", (req, res)=>{
+    const fecha = new Date()
+    res.send({fecha: fecha.toLocaleString()})
+})
+
+
+
+
+
+
+const server = app.listen(8080, ()=>{
+    console.log("Servidor express iniciado en el puerto 8080")
+})
+
+server.on("error", (error)=>{
+    console.log(`Error !!!: ${error}`)
+})
