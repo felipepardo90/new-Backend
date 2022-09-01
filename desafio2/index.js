2102601b1a90002aeb1313fc8c6154c237588419
Felipe Pardo <fpardo@UTESALTA.NET>
2022-09-01 15:47:57 -0300

    Limpieza de Cache

--- desafio2/index.js
+++ desafio2/index.js
@@ -8,7 +8,7 @@ class Container {
   async save(object) {
     // ? Recibe un objeto, lo guarda en el archivo y devuelve el id asignado
 
-    const dataToParse = await fs.readFileSync(this.file, "utf-8");
+    const dataToParse = await fs.promises.readFile(this.file, "utf-8");
     const dataParsed = JSON.parse(dataToParse);
     // * ¿El producto ya existe en el archivo?
     const productFound = dataParsed.find(({ title }) => title == object.title);
@@ -22,7 +22,7 @@ class Container {
         object.id = dataParsed.length + 1;
         dataParsed.push(object);
         const updatedFile = JSON.stringify(dataParsed, null, " ");
-        fs.writeFileSync(this.file, updatedFile);
+        fs.promises.writeFile(this.file, updatedFile);
         // ! console.log(updatedFile, "Aquí 23");
         console.log(
           `Se ha agregado el siguiente producto: ${object.title} con el id ${object.id}`
@@ -37,7 +37,7 @@ class Container {
   async getById(idEntered) {
     // ? Recibe un id y devuelve el objeto con ese id, o null si no está
 
-    const dataToParse = await fs.readFileSync(this.file, "utf-8");
+    const dataToParse = await fs.promises.readFile(this.file, "utf-8");
     const dataParsed = JSON.parse(dataToParse);
     // * ¿El producto ya existe en el archivo?
     const idFound = dataParsed.find(({ id }) => id === idEntered);
@@ -58,7 +58,7 @@ class Container {
   async getAll() {
     // ? Devuelve un array con los objetos presentes en el archivo
 
-    const dataToParse = await fs.readFileSync(this.file, "utf-8");
+    const dataToParse = await fs.promises.readFile(this.file, "utf-8");
     const dataParsed = JSON.parse(dataToParse);
 
     try {
@@ -76,7 +76,7 @@ class Container {
   async deleteById(idEntered) {
     // ? Elimina del archivo el objeto con el Id buscado
 
-    const dataToParse = await fs.readFileSync(this.file, "utf-8");
+    const dataToParse = await fs.promises.readFile(this.file, "utf-8");
     const dataParsed = JSON.parse(dataToParse);
     // * Se filtran los productos que no cumplen las condiciones (coincidir con el id proporcionado)
     const leakedID = dataParsed.filter(({ id }) => id !== idEntered);
@@ -90,7 +90,7 @@ class Container {
         );
         // * Se actualiza el archivo
         const updatedFile = JSON.stringify(leakedID, null, " ");
-        fs.writeFileSync(this.file, updatedFile);
+        fs.promises.writeFile(this.file, updatedFile);
       } else {
         console.log(`No se ha encontrado el objeto con id: ${idEntered}`);
       }
@@ -104,7 +104,7 @@ class Container {
     try {
       console.log("Todos los objetos fueron eliminados");
       // * Borrado de todos los objetos (Se sobreescribe el archivo a un array vacío)
-      await fs.writeFileSync(this.file, "[]");
+      await fs.promises.writeFile(this.file, "[]");
     } catch (error) {
       console.error(`Se ha producido un error en deleteAll: ${error}`);
     }
