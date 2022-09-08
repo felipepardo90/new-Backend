87a8aa63394cfeb9a3b22f85992fd1e18e4fd52f
Felipe Pardo <fpardo@UTESALTA.NET>
2022-09-08 16:29:54 -0300

    optimizando métodos

--- desafio4/Contenedor.js
+++ desafio4/Contenedor.js
@@ -5,10 +5,8 @@ class Container {
 
   // ? Recibe un objeto, lo guarda en el array y devuelve el id asignado ////
 
-  async save(object) {
+   save(object) {
     try {
-      // const dataToParse = await fs.promises.readFile(this.array, "utf-8");
-      // const dataParsed = JSON.parse(dataToParse);
       // * ¿El producto ya existe en el archivo?
       const productFound = this.array.find(
         ({ title }) => title === object.title
@@ -21,8 +19,6 @@ class Container {
         // * Si no existe, lo agrega y retorna el objeto con id asignado
         object.id = this.array.length + 1;
         this.array.push(object);
-        // const updatedFile = JSON.stringify(dataParsed, null, " ");
-        // fs.promises.writeFile(this.array, updatedFile);
         return object;
       }
     } catch (error) {
@@ -32,16 +28,12 @@ class Container {
 
   // ? Recibe un id y modifica el objeto con ese id, por un nuevo objeto ingresado
 
-  async update(idEntered, object) {
+   update(idEntered, object) {
     try {
       // * Se filtran los productos que no cumplen las condiciones (coincidir con el id proporcionado)
       const leakedID = this.array.filter(({ id }) => id != idEntered);
       // * Encuentra el producto con el id proporcionado
       const idFound = this.array.find(({ id }) => id == idEntered);
-      // // * ¿El producto ya existe en el archivo?
-      // const productFound = this.array.find(
-      //   ({ title }) => title === object.title
-      // );
 
       if (idFound) {
         const idFound = { ...object, id: idEntered };
@@ -59,10 +51,8 @@ class Container {
 
   // ? Recibe un id y devuelve el objeto con ese id, o null si no está  /////////
 
-  async getById(idEntered) {
+   getById(idEntered) {
     try {
-      // const dataToParse = await fs.promises.readFile(this.array, "utf-8");
-      // const dataParsed = JSON.parse(dataToParse);
       // * ¿El producto ya existe en el archivo?
       const idFound = this.array.find(({ id }) => id == idEntered);
 
@@ -79,13 +69,10 @@ class Container {
 
   // ? Devuelve un array con los objetos presentes en el archivo /////////////////
 
-  async getAll() {
+   getAll() {
     try {
-      // const dataToParse = await fs.promises.readFile(this.array, "utf-8");
-      // const dataParsed = JSON.parse(dataToParse);
 
       if (this.array.length > 0) {
-        // console.log(this.array);
         return this.array;
       } else {
         console.log("No hay elementos disponibles");
@@ -97,10 +84,8 @@ class Container {
 
   // ? Elimina del archivo el objeto con el Id buscado /////////////////////////////
 
-  async deleteById(idEntered) {
+   deleteById(idEntered) {
     try {
-      // const dataToParse = await fs.promises.readFile(this.array, "utf-8");
-      // const dataParsed = JSON.parse(dataToParse);
       // * Se filtran los productos que no cumplen las condiciones (coincidir con el id proporcionado)
       const leakedID = this.array.filter(({ id }) => id != idEntered);
       // * Encuentra el producto con el id proporcionado
@@ -110,9 +95,6 @@ class Container {
         console.log(
           `Se ha eliminado el objeto con id:${idEntered} >> [[${idFound.title}]]`
         );
-        // * Se actualiza el archivo
-        // const updatedFile = JSON.stringify(leakedID, null, " ");
-        // fs.promises.writeFile(this.array, updatedFile);
         this.array = leakedID;
         return idFound;
       } else {
@@ -123,17 +105,11 @@ class Container {
     }
   }
 
-  // ? Elimina todos los objetos presentes en el archivo ///////////////////////////////
-
-  async deleteAll() {
-    try {
-      console.log("Todos los objetos fueron eliminados");
-      // * Borrado de todos los objetos (Se sobreescribe el archivo a un array vacío)
-      await fs.promises.writeFile(this.array, "[]");
-    } catch (error) {
-      console.error(`Se ha producido un error en deleteAll: ${error}`);
-    }
+  deleteAll(){
+    console.log("Se han eliminado todos los productos")
+    this.array = []
   }
+
 }
 
 module.exports = Container;
