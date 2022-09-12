645e747e16e2ec601f66d36ee1b686310e23487e
Felipe Pardo <fpardo@UTESALTA.NET>
2022-09-12 16:32:45 -0300

    adaptando funcionalidad de los métodos

--- desafio5/API/Container.js
+++ desafio5/API/Container.js
@@ -17,8 +17,8 @@ class Container {
       );
 
       if (productFound) {
-        // * Si el producto ya existe, lo retorna 
-        return productFound;
+        // * Si el producto ya existe, lo retorna
+        return null;
       } else {
         // * Si no existe, lo agrega y retorna el objeto con id asignado
         object.id = dataParsed.length + 1;
@@ -29,6 +29,33 @@ class Container {
       }
     } catch (error) {
       console.error(`Se produjo un error en save:${error}`);
+    }
+  }
+
+  // ? Recibe un id y modifica el objeto con ese id, por un nuevo objeto ingresado
+
+  async update(idEntered, object) {
+    try {
+      const dataToParse = await fs.promises.readFile(this.file, "utf-8");
+      const dataParsed = JSON.parse(dataToParse);
+      // * Se filtran los productos que no cumplen las condiciones (coincidir con el id proporcionado)
+      const leakedID = dataParsed.filter(({ id }) => id != idEntered);
+      // * Encuentra el producto con el id proporcionado
+      const productFound = dataParsed.find(({ id }) => id == idEntered);
+
+      if (productFound) {
+        const productFound = { ...object, id: idEntered };
+        leakedID.push(productFound);
+        // * Se actualiza el archivo
+        const updatedFile = JSON.stringify(leakedID, null, " ");
+        fs.promises.writeFile(this.file, updatedFile);
+        console.log(`Producto ${idEntered} modificado con éxito`, productFound);
+        return productFound;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error(`Se produjo un error en saveById:${error}`);
     }
   }
 
@@ -45,7 +72,7 @@ class Container {
         console.log(`Se obtuvo el producto ${idFound.title}`);
         return idFound;
       } else {
-        console.log("No se han encontrado productos")
+        console.log("No se han encontrado productos");
       }
     } catch (error) {
       console.error(`Se produjo un error en getByID: ${error}`);
@@ -87,7 +114,7 @@ class Container {
         // * Se actualiza el archivo
         const updatedFile = JSON.stringify(leakedID, null, " ");
         fs.promises.writeFile(this.file, updatedFile);
-        return idFound
+        return idFound;
       } else {
         console.log(`No se ha encontrado el objeto con id: ${idEntered}`);
       }
