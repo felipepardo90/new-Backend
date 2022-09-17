22f63912c7469ddbfe5afab9943af40622d726a3
Felipe Pardo <felipepardo90@hotmail.com>
2022-09-17 15:19:47 -0300

    fixing layouts

--- desafio5/server.js
+++ desafio5/server.js
@@ -31,8 +31,8 @@ app.engine(
 
 //? VIEW ENGINES /////////////////////////////////////
 
-// !app.set('view engine', 'pug')
-// ! app.set('view engine', 'ejs')
+//! app.set('view engine', 'pug')
+//! app.set('view engine', 'ejs')
 ! app.set('view engine', 'hbs')
 
 //? ROUTES ///////////////////////////////////////////
