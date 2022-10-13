d135e98b795f35665edb8d8694b65d88f8bf3b11
Felipe Pardo <fpardo@UTESALTA.NET>
2022-10-13 15:53:47 -0300

    new folders db, libs and new dependencies

---
+++ desafio7/src/db/db.js
@@ -0,0 +1,12 @@
+export const connection = {
+  client: "mysql",
+  connection: {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password,
+    database: "db-coder",
+  },
+};
+
+// export default connection
