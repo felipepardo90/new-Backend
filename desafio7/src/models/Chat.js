4138d29492586bbb9533494588b1ece68d69f36f
Felipe Pardo <fpardo@UTESALTA.NET>
2022-10-18 16:23:00 -0300

    Chat's model methods

--- desafio7/src/models/Chat.js
+++ desafio7/src/models/Chat.js
@@ -1,21 +1,25 @@
 import knex from "knex";
 
 export default class Messages {
-  constructor(file) {
-    this.file = file;
+  constructor(config, table) {
+    this.knex = knex(config);
+    this.table = table;
   }
 
   async saveMessage(object) {
     try {
-      const messageToParse = await fs.promises.readFile(this.file, "utf-8");
-      const messages = JSON.parse(messageToParse);
-
-      object.id = messages.length + 1;
-      messages.push(object);
-      const updatedFile = JSON.stringify(messages, null, " ");
-      fs.promises.writeFile(this.file, updatedFile);
-
+      await this.knex.insert(object).into(this.table);
+      const messages = await this.knex.select("*").from(this.table);
       return messages;
+      // const messageToParse = await fs.promises.readFile(this.file, "utf-8");
+      // const messages = JSON.parse(messageToParse);
+
+      // object.id = messages.length + 1;
+      // messages.push(object);
+      // const updatedFile = JSON.stringify(messages, null, " ");
+      // fs.promises.writeFile(this.file, updatedFile);
+
+      // return messages;
     } catch (error) {
       console.error(`Se produjo un error en saveMessage:${error}`);
     }
@@ -23,9 +27,10 @@ export default class Messages {
 
   async readMessages() {
     try {
-      const messageToParse = await fs.promises.readFile(this.file, "utf-8");
-      const messages = JSON.parse(messageToParse);
-      return messages;
+      return await this.knex.select("*").from(this.table);
+      // const messageToParse = await fs.promises.readFile(this.file, "utf-8");
+      // const messages = JSON.parse(messageToParse);
+      // return messages;
     } catch (error) {
       console.error(`Se produjo un error en readMessages:${error}`);
     }
