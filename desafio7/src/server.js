b99fa64df2d97b521b9f6529a4818258926a5572
Felipe Pardo <fpardo@UTESALTA.NET>
2022-10-18 16:25:51 -0300

    Finalizing DB connection and socket configurations

--- desafio7/src/server.js
+++ desafio7/src/server.js
@@ -5,13 +5,14 @@ import http from "http";
 const server = http.createServer(app);
 const io = new WebSocketServer(server);
 //! DATABASE
-import MySQLConn from "./db/connection.js";
+import MySQLConn from "./DB/mysql/connection.js";
+import SQLiteConn from "./DB/sqlite/connection.js"
 //! CONTENEDOR PRODUCTOS
 import Container from "./models/Container.js";
 const DBprod = new Container(MySQLConn, "Products");
 //! CONTENEDOR MENSAJES
 import Messages from "./models/Chat.js";
-const DBmsg = new Messages(MySQLConn, "Messages");
+const DBmsg = new Messages(SQLiteConn, "Messages");
 //! STARTING SERVER
 
 server.listen(app.get("port"), () => {
@@ -28,36 +29,37 @@ io.on("connection", async (socket) => {
   //! Nueva conexión
   console.log(`New Connection: ${socket.id}`);
 
-  // //! PRODUCTOS
+  //! PRODUCTOS
 
-  // const products = await DBprod.getAll();
-  // socket.emit("product:all", products);
+  const products = await DBprod.getAll();
+  socket.emit("product:all", products);
 
-  // // //! Guardar producto
-  // socket.on("product:new", async (object) => {
-  //   const data = await DBprod.save(object);
-  //   data === null
-  //     ? socket.emit("product:submit", { product: object, status: true })
-  //     : socket.emit("product:submit", { product: object, status: false });
-  //   io.sockets.emit("product:all", products);
-  // });
+  //! Guardar producto
+  socket.on("product:new", async (object) => {
+    const data = await DBprod.save(object);
+    data === null
+      ? socket.emit("product:submit", { product: object, status: true })
+      : socket.emit("product:submit", { product: object, status: false });
+    io.sockets.emit("product:all", products);
+  });
 
-  // //! CHAT
+  //! CHAT
 
-  // //! El evento chat:messages iniciará enviando el array existente al cliente
-  // const allMessages = await DBmsg.readMessages();
-  // socket.emit("chat:history", allMessages);
+  //! El evento chat:messages iniciará enviando el array existente al cliente
+  const allMessages = await DBmsg.readMessages();
+  console.log(allMessages, "server")
+  socket.emit("chat:history", allMessages);
 
-  // //! Se escucha el evento chat:message, se guarda el mensaje recibido por el cliente y se emite un mensaje general con el array Messages actualizado a todos los sockets conectados y por conectarse
+  //! Se escucha el evento chat:message, se guarda el mensaje recibido por el cliente y se emite un mensaje general con el array Messages actualizado a todos los sockets conectados y por conectarse
 
-  // socket.on("chat:message", (data) => {
-  //   DBmsg.saveMessage(data);
-  //   io.sockets.emit("chat:history", allMessages);
-  // });
+  socket.on("chat:message", (data) => {
+    DBmsg.saveMessage(data);
+    io.sockets.emit("chat:history", allMessages);
+  });
 
-  // //! Se escucha el evento chat:typing y se emite un mensaje a todos los sockets conectados, excepto al que "está escribiendo..." con el método broadcast
+  //! Se escucha el evento chat:typing y se emite un mensaje a todos los sockets conectados, excepto al que "está escribiendo..." con el método broadcast
 
-  // socket.on("chat:typing", (data) => {
-  //   socket.broadcast.emit("chat:typing", data);
-  // });
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
 });
