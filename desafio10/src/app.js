59c70005a8f31ca677a0232bac8fdb0364c23cc8
Felipe Pardo <felipepardo90@hotmail.com>
2022-11-21 22:10:29 -0300

    modifying app

--- desafio10/src/app.js
+++ desafio10/src/app.js
@@ -3,18 +3,12 @@ import session from "express-session";
 //* Mongo Connect
 import MongoStore from "connect-mongo";
 const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+import { MONGODB_URI, PORT, __dirname } from "./config.js";
 import bodyParser from "body-parser";
-import morgan from "morgan";
-import morgan from "morgan";
 import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-import * as dotenv from "dotenv";
-dotenv.config()
+import morgan from "morgan";
+
 const app = express();
-
-
 
 //! ROUTES
 
@@ -22,7 +16,7 @@ import indexRoute from "./routes/index.routes.js";
 
 //! SETTINGS
 
-app.set("port", process.env.PORT); //! CONFIG port
+app.set("port", PORT); //! CONFIG port
 app.set("json spaces", 2); //! JSON formatter
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs"); //! VIEW ENGINES
@@ -38,15 +32,16 @@ app.use("/", indexRoute); //
 app.use(
   session({
     secret: "12341felipao512313",
-    resave: "false",
+    store: MongoStore.create({
+      mongoUrl: MONGODB_URI,
+      mongoOptions,
+    }),
+    resave: false,
     saveUninitialized: false,
     cookie: {
       maxAge: 10 * 1000 * 60,
     },
-    store:MongoStore.create({
-        mongoUrl:`mongodb+srv://${process.env.USER}:${process.env.PASS}@codercluster.exshfro.mongodb.net/?retryWrites=true&w=majority`,
-        mongoOptions
-    })
-}))
+  })
+);
 
 export default app;
