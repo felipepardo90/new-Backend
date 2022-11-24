de7a6d3de875daa79dee59cbd99bf7d61da302c0
Felipe Pardo <felipepardo90@hotmail.com>
2022-11-24 00:23:52 -0300

    passport dependencies - new views

--- desafio10/src/routes/index.routes.js
+++ desafio10/src/routes/index.routes.js
@@ -1,12 +1,35 @@
 import { Router } from "express";
 const router = Router();
-import productsRoute from "./products.routes.js"
-import controller from "../controllers/index.controller.js"
+import productsRoute from "./products.routes.js";
+import passport from "passport";
+import controller from "../controllers/index.controller.js";
 
+let user = {
+  name: "Fabiola",
+};
 
 //? INDEX
 router.use("/api/products", productsRoute);
-router.get("/", controller.index)
+// router.get("/", controller.index)
+router.get("/", (req, res, next) => {
+  res.render("index", { user });
+});
 
+router.get("/signup", (req, res, next) => {
+  res.render("register");
+});
 
-export default router
+router.post(
+  "/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    passReqToCallback: true,
+  })
+);
+
+router.get("/signin", (req, res, next) => {});
+
+router.post("/signin", (req, res, next) => {});
+
+export default router;
