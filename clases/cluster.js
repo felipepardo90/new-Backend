75f47aa694513381007589cfabb42ffc19206a66
Felipe Pardo <felipepardo90@hotmail.com>
2022-12-02 00:21:34 -0300

    clases cluster y normalize

---
+++ clases/cluster.js
@@ -0,0 +1,23 @@
+import cluster from "cluster";
+import { cpus } from "os";
+import express from "express";
+
+const app = express();
+const cpu = cpus();
+console.log(cpu.length);
+
+console.log(cluster.isPrimary);
+
+if (cluster.isPrimary) {
+  console.log(`Primary: ${process.pid}`);
+  for(let i = 0; i <cpu.length;i++)
+  cluster.fork();
+
+cluster.on("exit", (worker, code, signal)=>{
+    console.log(`Worker with id ${worker.process.pid} killed`)
+})
+
+
+} else {
+  console.log(`Worker with id ${process.pid}`);
+}
--- clases/normalize.js
+++ clases/normalize.js
@@ -53,3 +53,73 @@ console.log(
   "INSPECT"
 );
 
+
+//* COPIAAAAAAR ESTOOOO
+
+import { normalize, schema, denormalize } from "normalizr";
+import util from "util";
+
+const data = [{
+  id:"1",
+  author: {
+    id: "felipao@",
+    name: "Felipe",
+    lastname: "Pardo",
+    age: 32,
+    alias: "felipao",
+    avatar: "feli.avatar",
+  },
+  text: "Hola, Holaaaa",
+  date: new Date().toLocaleString(),
+}, {
+  id:"2",
+  author: {
+    id: "fabiola@",
+    name: "Fabiii",
+    lastname: "Maray",
+    age: 29,
+    alias: "test",
+    avatar: "test",
+  },
+  text: "Hola,cómo estás",
+  date: new Date().toLocaleString(),
+}];
+
+// const authorSchema = new schema.Entity("authors");
+// const authorSchema = new schema.Entity("authors",{}, {idAttribute: "email"});
+const authorSchema = new schema.Entity("authors");
+const commentSchema = new schema.Entity("comments");
+const authors = [authorSchema]
+
+//* NORMALIZE
+const normalizedMsg = normalize(data, authors);
+console.log(
+  ">---------------",
+  util.inspect(normalizedMsg, false, 8, true),
+  // normalizedMsg,
+  ">--------------->"
+);
+// const normObj = normalize(blogspot, postSchema);
+// console.log(postSchema)
+// console.log(normObj);
+// console.log(util.inspect(normObj, false, 12, true))
+// console.log(`normalize: ${JSON.stringify(normObj).length}`)
+
+//*
+
+//* DENORMALIZE
+
+// const denormalized = denormalize(
+//   normalizedMsg.result,
+//   authors,
+//   normalizedMsg.entities
+// );
+// console.log(
+//   ">---------------",
+//   console.log(denormalized),
+//   util.inspect(denormalized, false, 8, true),
+//   ">--------------->"
+// );
+// console.log(`denormaliza: ${JSON.stringify(denormObj).length}`)
+
+//*
