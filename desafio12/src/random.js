6f309c485164420e93e7ed679d6af0353608a8a4
Felipe Pardo <felipepardo90@hotmail.com>
2022-12-20 23:46:05 -0300

    fixing random function

--- desafio12/src/random.js
+++ desafio12/src/random.js
@@ -1,16 +1,17 @@
 let randomList = [];
+let counter = 1;
 function randomNumber(max, min) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 process.on("message", (totalQty) => {
-  let num, keyNumExists;
+  let num, keyNum, keyNumExists;
   for (let i = 0; i < totalQty; i++) {
-    num = randomNumber(1, 1000);
-    keyNumExists = randomList.findIndex((obj) => obj[num] === num);
+    keyNum = (obj) => obj[num];
+    keyNumExists = randomList.findIndex(keyNum);
     keyNumExists === -1
-      ? randomList.push({ [num]: 1 })
-      : (randomList[keyNumExists][num] += 1);
+      ? randomList.push({ [num]: counter })
+      : (randomList[keyNumExists][num] += counter);
   }
+
   process.send(randomList);
 });
-//TODO cambiar esto
