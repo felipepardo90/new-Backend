fb32a1eedc296bff9a192a87c5ff1d599a605acf
Felipe Pardo <felipepardo90@hotmail.com>
2023-01-29 20:44:33 -0300

    fixed untracked files

--- desafio10/src/db/sqlite/connection.js
+++
@@ -1,9 +0,0 @@
-const connection = {
-  client: "sqlite3",
-  connection: {
-    filename: "./desafio7/src/DB/ecommerce.sqlite3",
-  },
-  useNullAsDefault:true
-};
-
-export default connection
--- desafio9/src/routes/faker.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import fakerTest from "../controllers/faker.controller.js"
-
-
-router.get("/", fakerTest);
-
-export default router;
