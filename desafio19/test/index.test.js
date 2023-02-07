b60ce51c6903f9d0874c22ff842cbee40a777f66
Felipe Pardo <fpardo@UTESALTA.NET>
2023-02-07 10:20:56 -0300

    fixing tests on supertest

--- desafio19/test/index.test.js
+++ desafio19/test/index.test.js
@@ -21,80 +21,88 @@ const newProduct = {
 
 //! Get all products
 
-// describe("get all /api/products/ ", () => {
-//   it("should try to get all products", async (done) => {
-//     await request(api)
-//       .get("/api/products")
-//       // .auth("Felipe", "123456")
-//       .set("Accept", "Application/json")
-//       .expect("Content-type", /json/)
-//       .expect(200, done());
-//   });
-// });
+describe("GET all products /api/products ", () => {
+  it("should try to get all products", async (done) => {
+    await request(api)
+      .get("/api/products")
+      // .auth("Felipe", "123456")
+      .set("Accept", "Application/json")
+      .expect("Content-type", /json/)
+      .expect(200, done());
+  });
+});
 
 //! Get product by id
 
-// describe("GET product on /api/products", () => {
-//   //
-//   it("respond a single product with id", (done) => {
-//     request(api)
-//       .get("/api/products/63d810826edc965e178c6832")
-//       .set("Accept", "Application/json")
-//       .expect("Content-type", /json/)
-//       .expect(200, done());
-//   });
+describe("GET product by ID on /api/products", () => {
+  //
+  it("GET PRODUCT succesfully", (done) => {
+    request(api)
+      .get("/api/products/63d810826edc965e178c6832")
+      .set("Accept", "Application/json")
+      .expect("Content-type", /json/)
+      .expect(200, done());
+  });
 
-//   // TODO Resolver para cuando no encuentra el producto
+  //! Resuelto para cuando no encuentra el producto
 
-//   it("respond when the product doesnt exists on /api/products", (done) => {
-//     request(api)
-//       .get("/api/products/aleatoryId")
-//       .set("Accept", "Application/json")
-//       .expect("Content-type", /json/)
-//       .expect(404)
-//       .expect({ error: "Producto no encontrado" })
-//       .end((err) => {
-//         if (err) return done(err);
-//         done();
-//       });
-//   });
-// });
+  it("GET PRODUCT failed request on /api/products", (done) => {
+    request(api)
+      .get("/api/products/aleatoryId")
+      .set("Accept", "Application/json")
+      .expect("Content-type", /json/)
+      .expect(404)
+      .expect({ error: "Producto no encontrado" })
+      .end(done());
+  });
+});
 
 //! Add product
 
-// describe("POST products on /api/products", () => {
-//   it("respond with 201 item created", async (done) => {
-//     const item = {
-//       title: "prueba",
-//       price: 11,
-//       thumbnail: "prueba.jpg",
-//       description: "lalalala",
-//       code: "A1111",
-//       stock: 1,
-//     };
-//     await request(api)
-//       .post("/api/products")
-//       .send(item)
-//       .set("Accept", "Application/json")
-//       .expect("Content-type", /html/)
-//       .expect(201, done())
-//       .end((err) => {
-//         if (err) return done(err);
-//         done();
-//       });
-//   });
-// });
+describe("POST - add a product on /api/products", () => {
+  it("respond with 201 item created", async (done) => {
+    const item = {
+      title: "prueba",
+      price: 11,
+      thumbnail: "prueba.jpg",
+      description: "lalalala",
+      code: "A1111",
+      stock: 1,
+    };
+    await request(api)
+      .post("/api/products")
+      .send(item)
+      .set("Accept", "Application/json")
+      .expect("Content-type", /html/)
+      .expect(201, done())
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
 
 //! Change product
 
-// it("PUT || Modify product respond with 200", async (done) => {
-//   await request(api)
-//     .put("/api/products/63d816ef8d0c6320ba40406e")
-//     .send(newProduct)
-//     .set("Accept", "Application/json")
-//     .expect(200)
-//     .end((err) => {
-//       if (err) return done(err);
-//       done();
-//     });
-// });
+describe("PUT - modify product on /api/products", () => {
+  it("Success respond with 200", (done) => {
+    request(api)
+      .put("/api/products/63d816ef8d0c6320ba40406e")
+      .send(newProduct)
+      .set("Accept", "Application/json")
+      .expect(200)
+      .end(done());
+  });
+});
+
+//! Change product
+
+describe("DELETE product on api/products", () => {
+  it("Success respond with 200", (done) => {
+    request(api)
+      .delete("/api/products/63d816ef8d0c6320ba40406e")
+      .set("Accept", "Application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done());
+  });
+});
