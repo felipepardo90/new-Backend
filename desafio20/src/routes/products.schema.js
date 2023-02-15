f4cac80fe17d8a03ce0c9326ba03cfbed0400975
Felipe Pardo <fpardo@UTESALTA.NET>
2023-02-15 16:51:45 -0300

    products schema

--- desafio20/src/routes/products.schema.js
+++ desafio20/src/routes/products.schema.js
@@ -2,9 +2,9 @@ export default `
 
 type Product {
 
-    _id: String!
-    title: String!, 
-    price: Int!, 
+    _id: String!,
+    title: String, 
+    price: Int, 
     thumbnail: String, 
     description: String,
     code: String, 
@@ -12,10 +12,31 @@ type Product {
 
 }
 
-type Query {
-getProductById(id: String!): Product
-getAllProducts: [Product!]!
+input productInput {
+
+    title: String, 
+    price: Int, 
+    thumbnail: String, 
+    description: String,
+    code: String, 
+    stock: Int
+    
 }
 
+type Query {
+
+    getProductById(id: String!): Product
+    getAllProducts: [Product!]!,
+    
+
+}
+
+type Mutation {
+
+    saveProduct(data: productInput!): Product,
+    updateProduct(id: String!, data: productInput!): Product,
+    deleteProduct(id: String!): Product
+
+}
 
 `;
