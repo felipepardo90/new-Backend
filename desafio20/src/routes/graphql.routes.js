b344ca8f2aa0ce64143ee5786c449c0aac261450
Felipe Pardo <fpardo@UTESALTA.NET>
2023-02-15 16:52:04 -0300

    graph ql route

--- desafio20/src/routes/graphql.routes.js
+++ desafio20/src/routes/graphql.routes.js
@@ -2,30 +2,16 @@ import { Router } from "express";
 const router = Router();
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
-import { DAOProducts } from "../daos/DAO.Factory.js";
-import typeDef from "./products.schema.js";
+import typeDefinitions from "./products.schema.js";
+import resolvers from "./products.resolvers.js";
 
-const productsSchema = buildSchema(typeDef);
-
-async function getProductById({ id }) {
-  console.log(await DAOProducts.getById("635322adedbfa68cab085f35"));
-  const product = await DAOProducts.getById(id);
-  return product;
-}
-
-async function getAllProducts() {
-  console.log(await DAOProducts.getAll());
-  // const product = await DAOProducts.getById(id);
-  // product._id = product._id.toString();
-  // return product;
-}
-
+const productsSchema = buildSchema(typeDefinitions);
 
 router.use(
   "/graphql",
   graphqlHTTP({
     schema: productsSchema,
-    rootValue: { getProductById, getAllProducts },
+    rootValue: resolvers,
     graphiql: true,
   })
 );
