c5c7b555856a804be5bfd9f578087baa43949300
Felipe Pardo <fpardo@UTESALTA.NET>
2023-02-16 16:27:50 -0300

    API server SAILS - packages

---
+++ desafio21/ecom-project/api/controllers/CartsController.js
@@ -0,0 +1,12 @@
+/**
+ * CartsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
