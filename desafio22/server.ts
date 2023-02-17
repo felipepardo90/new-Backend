beac432d348a9ed7348f2bcc84594f9f6806e157
Felipe Pardo <fpardo@UTESALTA.NET>
2023-02-17 17:06:22 -0300

    server on deno

---
+++ desafio22/server.ts
@@ -0,0 +1,3 @@
+import { createApp } from "https://deno.land/x/servest@v1.3.1/mod.ts"
+import React from "https://dev.json.io/react/index.js"
+import ReactDOMServer from "https://dev.json.io/react-dom/server.js"
