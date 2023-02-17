8f5ef372a69e4f9555c25cee82d82382197fea0a
Felipe Pardo <felipepardo90@hotmail.com>
2023-02-17 00:07:54 -0300

    fixed untracked files

--- desafio19/src/dto/mongo.DTO.carts.js
+++
@@ -1,8 +0,0 @@
-import DTOProduct from "./mongo.DTO.products";
-
-export default class DTOCart {
-  constructor(products) {
-    this.products = products.map((product) => DTOProduct(product));
-    this.subtotal = products.reduce((prev, next) => prev + next.price, 0);
-  }
-}
--- desafio20/src/dto/mongo.DTO.products.js
+++
@@ -1,9 +0,0 @@
-const DTOProduct = ({ title, price, thumbnail, description }) => {
-  return {
-    title,
-    price,
-    thumbnail,
-    description,
-  };
-};
-export default DTOProduct;
